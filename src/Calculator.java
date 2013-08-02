3907457ea87fe077629226565c5e87e3491e6838
Kush Jain <Mr.KushJain@gmail.com>
2013-08-02 19:05:44 +0530

    Test for Multiplication and Addition Operations

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created with IntelliJ IDEA.
  * User: kush
@@ -7,10 +9,55 @@
  */
 public class Calculator {
 
-
-    public int evaluate(String input) {
-
-        String [] terms = input.split(",");
-        return Integer.parseInt(terms[0]);
+    public static final char MULTIPLY = '*';
+    public static final char ADDITION = '+';
+    private static int NOT_FOUND = -1;
+    public int getNumber(String input,Integer startPosition){
+        StringBuffer numberInString = new StringBuffer();
+        int i;
+        for(i=startPosition;i<input.length() && Character.isDigit(input.charAt(i));i++){
+            numberInString.append(input.charAt(i));
+        }
+        startPosition = i;
+        try {
+            return Integer.parseInt(numberInString.toString());
+        } catch(NumberFormatException e) {
+            return NOT_FOUND;
+        }
     }
+    public int evaluate(String input){
+
+        Integer result=0;
+        int findPosition=0;
+        char operation = getOperation(input);
+        for(int i=0;i<input.length();i++)
+        {
+            Integer number = getNumber(input,findPosition);
+            findPosition += getNextPosition(number);
+            if(number==NOT_FOUND) {
+                         break;
+            }
+            result = performOperation(result,number,operation);
+        }
+        return result;
+    }
+
+    private int getNextPosition(Integer number) {
+        return String.valueOf(number).length()+1;
+    }
+
+    private char getOperation(String input) {
+        return input.charAt(input.length()-1);
+    }
+
+    public int performOperation(Integer result, Integer number, char operation) {
+        if(operation== ADDITION){
+            result = result + number;
+        }
+        if(operation== MULTIPLY){
+            result = result * number;
+        }
+        return result;
+    }
+
 }
--- tests/CalculatorTests.java
+++ tests/CalculatorTests.java
@@ -10,8 +10,49 @@ import static org.junit.Assert.assertEquals;
  * To change this template use File | Settings | File Templates.
  */
 public class CalculatorTests {
-
+//
     Calculator calculator = new Calculator();
+
+
+    @Test
+    public void testgetNumberFortyFiveCommaGivesFortyFive() throws Exception {
+        assertEquals(45,calculator.getNumber("45,",0));
+
+    }
+
+    @Test
+      public void testgetNumberSixtyFiveCommaGivesSixtyFive() throws Exception {
+        assertEquals(65,calculator.getNumber("65,",0));
+
+    }
+    @Test
+    public void testgetNumberSixtyFiveDollarGivesSixtyFive() throws Exception {
+        assertEquals(65,calculator.getNumber("65$,",0));
+
+    }
+
+    @Test
+    public void testPerformOperationFiveSixPlusGivesEleven() throws Exception {
+        assertEquals(11,calculator.performOperation(5,6,'+'));
+
+    }
+
+    @Test
+    public void testPerformOperationTwoThreePlusGivesEleven() throws Exception {
+        assertEquals(5,calculator.performOperation(2,3,'+'));
+
+    }
+    @Test
+    public void testPerformOperationFiveSixMultiplyGivesEleven() throws Exception {
+        assertEquals(30,calculator.performOperation(5,6,'*'));
+
+    }
+
+    @Test
+    public void testPerformOperationTwoThreeMultiplyGivesEleven() throws Exception {
+        assertEquals(6,calculator.performOperation(2,3,'*'));
+
+    }
     @Test
     public void testEvaluationOfFortyFiveCommaPlusIsFortyFive(){
            assertEquals(45,calculator.evaluate("45,+"));
@@ -20,4 +61,18 @@ public class CalculatorTests {
     public void testEvaluationOfSixtyFiveCommaPlusIsSixtyFive(){
         assertEquals(65,calculator.evaluate("65,+"));
     }
+    @Test
+    public void testEvaluationOfFiftySixtyCommaSixtyPlusIsHunderedAndTen(){
+        assertEquals(110,calculator.evaluate("50,60,+"));
+    }
+    public void testEvaluationOfFortyFiveCommaEightyNineCommaOnePlusIsOneHunderedThirtyFive(){
+        assertEquals(135,calculator.evaluate("45,89,1,+"));
+    }
+    public void testEvaluationOfThreeDashFourDashSixDashAsterixIsSeventyTwo(){
+        assertEquals(72,calculator.evaluate("3-4-6-*"));
+    }
+    public void testEvaluationOfThreeCommaFourDashSixDashAsterixIsSeventyTwo(){
+        assertEquals(72,calculator.evaluate("3,4-6-*"));
+    }
+
 }
